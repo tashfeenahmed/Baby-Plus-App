d66c39714cb63c95c882660ab7f52b9498266812
Tashfeen Ahmed <tashfene@gmail.com>
2015-12-08 14:41:03 +0500

    Timeline initial setup

--- www/js/app.js
+++ www/js/app.js
@@ -40,14 +40,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
+  .state('timeline', {
+    url: '/timeline',
+    templateUrl: 'templates/tab-dash.html',
+    controller: 'timelineCtrl'
   })
 
   .state('tab.chats', {
@@ -80,6 +76,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/timeline');
 
 });
